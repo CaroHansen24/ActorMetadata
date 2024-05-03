455da83f43e3318fb1a0d0661df1574f72c8f6e3
carolinelillelund <carolinelillelund@hotmail.com>
2024-05-04 01:51:05 +0200

    Set up demo level and fix small bug when adding an existing liked actor

--- Source/ActorMetadata/Private/MyGameInstance.cpp
+++ Source/ActorMetadata/Private/MyGameInstance.cpp
@@ -4,7 +4,13 @@
 #include "MyGameInstance.h"
 
 void UMyGameInstance::addToRecentLikedActors(AActor* actor) {
+	// If the actor was liked previously it should be removed from
+	// its previous index and moved to the front of the array.
+	if (recentlikedActors.Contains(actor)) {
+		recentlikedActors.Remove(actor);
+	}
 	recentlikedActors.Add(actor);
+
 	if (recentlikedActors.Num() > 5) {
 		recentlikedActors.RemoveAt(0);
 	}
